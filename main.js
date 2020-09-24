ff6d30db32ff2113c2b46356c932966dec9fa335
fadi <f.alfuqahaaa@gmail.com>
2020-09-24 06:15:17 +0100

    modify show hide methode

--- main.js
+++ main.js
@@ -1,31 +1,62 @@
-const thumbnail = (src) => {
-    const $div = $("<div>", { "class": "thumbnail" });
-    const $img = $("<img>", { "src": src });
-    $div.append($img);
-    return $div;
-}
+$(function () {
 
-const info = (textHead,textp) => {
-    const $div = $("<div>", { "class": "info" });
-    const head = $("<h3>");
-    head.append(textHead);
-    $div.append(head);
-    const paragraph = $("<p>");
-    paragraph.append(textp);
-    $div.append(paragraph);
-    return $div;
+    const thumbnail = (src) => {
+        const $div = $("<div>", { "class": "thumbnail" });
+        const $img = $("<img>", { "src": src });
+        $div.append($img);
+        return $div;
+    }
 
-}
+    const info = (textHead, textp) => {
+        const $div = $("<div>", { "class": "info" });
+        const head = $("<h3>");
+        head.append(textHead);
+        $div.append(head);
+        const paragraph = $("<p>");
+        paragraph.append(textp);
+        $div.append(paragraph);
+        return $div;
 
-const videoItem = (obj) =>  {
-    const rightSide = $(".rightSide");
-    const $div = $("<div>", { id: obj.id, "class": "selectedVedio", "title": obj.title });
-    $div.append((thumbnail(obj.thumbnail)));
-    $div.append(info(obj.title, obj.title));
-    rightSide.append($div);
-}
+    }
 
+    const videoItem = (obj) => {
+        const rightSide = $(".rightSide");
+        const $div = $("<div>", { id: obj.id, "class": "selectedVedio", "title": obj.title });
+        $div.on('click', function () {
+            const videoShow = $("iframe");
+            videoShow.attr("src", `https://www.youtube.com/embed/${this.id}`);
+            titleDiv = $(".rightSide .description");
+            titleDiv.html(this.title);
+            channelDiv = $(".title div h4");
+            channelDiv.html(this.title);
+            showHide(this.id)
+        })
+        $div.append((thumbnail(obj.thumbnail)));
+        $div.append(info(obj.title, obj.title));
+        rightSide.append($div);
+    }
 
-videos.forEach(element => {
-    videoItem(element);
+    videoShow = $("iframe");
+    videoShow.attr("src", `https://www.youtube.com/embed/${videos[6].id}`);
+    videos.forEach(element => {
+        videoItem(element);
+    });
+
+    const showHide = (id) => {
+        console.log(id)
+        const items = $(".selectedVedio")
+        items.each((index, value) => {
+            console.log(value.id)
+            console.log(value.id === id)
+            console.log( $(this))
+            if (value.id === id) {
+                $(this).hide();
+            }
+            else {
+                $(this).show();
+            }
+        })
+    }
+  
+
 });
